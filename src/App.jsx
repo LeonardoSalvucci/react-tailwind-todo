953518f30ad7f2c16631c0e29d1c36c4329d45a3
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-09 14:19:32 -0300

    Add state to handle tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "./components/Container";
 import TaskItem from "./components/TaskItem";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [tasks, setTask] = useState([
+    {
+      task: "Learn React",
+      done: false,
+    },
+    {
+      task: "Learn Typescript",
+      done: false,
+    },
+    {
+      task: "Learn React",
+      done: true,
+    },
+  ]);
+
   return (
     <div className="App flex bg-gradient-to-r from-blue-500 to-teal-200 justify-center items-center h-screen w-screen">
       <Container>
@@ -12,13 +27,21 @@ function App() {
           <AddTask />
 
           {/* tasks items */}
-          <TaskItem task="learn react" />
-          <TaskItem task="learn typescript" />
+          {tasks
+            .filter((task) => !task.done)
+            .map((task, index) => (
+              <TaskItem key={`task-${index}`} task={task.task} />
+            ))}
 
           {/* separator */}
           <div className="w-full border border-blue-300"></div>
 
-          <TaskItem task="learn react" done />
+          {/* done tasks */}
+          {tasks
+            .filter((task) => task.done)
+            .map((task, index) => (
+              <TaskItem key={`task-${index}`} task={task.task} done />
+            ))}
         </div>
       </Container>
     </div>
