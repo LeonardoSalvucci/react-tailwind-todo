2ce9fa099d692371186722ec3afcf72bf139ad20
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-09 14:08:07 -0300

    Adding overflow to container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 export default function ({ children }) {
   return (
-    <div className="bg-blue-200 rounded-xl w-1/2 h-1/2 flex justify-center">
+    <div className="bg-blue-200 rounded-xl w-1/2 h-1/2 flex justify-center overflow-scroll">
       {children}
     </div>
   );
